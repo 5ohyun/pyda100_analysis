5925a6391c9b193505f0f71b7d7571ef0859127f
5ohyun <leeso226@naver.com>
2021-02-18 21:48:46 +0900

    물류 네트워크 설계 - 생산 계획 (수정중 2)

--- chap7-2.py
+++ chap7-2.py
@@ -64,3 +64,52 @@ df_plan_sol
   
 value(m.objective) # 총 이익
 
+# 제약 조건 계산 함수
+
+def condition_stock(df_plan,df_material,df_stock):
+    flag = np.zeros(len(df_material.columns))
+    for i in range(len(df_material.columns)):  # 원료
+        temp_sum = 0
+        for j in range(len(df_material.index)):  # 제품
+            temp_sum = temp_sum + df_material.iloc[j][i]*float(df_plan.iloc[j]) # 필요한 원료 개수
+        if (temp_sum<=float(df_stock.iloc[0][i])):
+            flag[i] = 1
+            
+        print(df_material.columns[i]+"  사용량:"+str(temp_sum)+", 재고:"+str(float(df_stock.iloc[0][i])))
+    return flag
+
+print("제약 조건 계산 결과:"+str(condition_stock(df_plan_sol,df_material,df_stock)))
+
+
+
+
+제품 = list('AB')
+대리점 = list('PQ')
+공장 = list('XY')
+레인 = (2,2)
+
+# 운송비 #
+tbdi = pd.DataFrame(((j,k) for j in 대리점 for k in 공장), columns=['대리점','공장'])
+tbdi['운송비'] = [1,2,3,1]
+print(tbdi)
+
+# 수요 #
+tbde = pd.DataFrame(((j,i) for j in 대리점 for i in 제품), columns=['대리점','제품'])
+tbde['수요'] = [10,10,20,20]
+print(tbde)
+
+# 생산 #
+tbfa = pd.DataFrame(((k,l,i,0,np.inf) for k,nl in zip (공장,레인) for l in range(nl) for i in 제품), 
+                    columns=['공장','레인','제품','하한','상한'])
+tbfa['생산비'] = [1,np.nan,np.nan,1,3,np.nan,5,3]
+tbfa.dropna(inplace=True)
+tbfa.loc[4,'상한']=10
+print(tbfa)
+
+from ortoolpy import logistics_network
+_, tbdi2, _ = logistics_network(tbde, tbdi, tbfa,dep = "대리점", dem = "수요",fac = "공장",
+                                prd = "제품",tcs = "운송비",pcs = "생산비",lwb = "하한",upb = "상한")
+
+print(tbfa)
+print(tbdi2)
+
